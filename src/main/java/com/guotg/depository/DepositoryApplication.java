d1926ae49b2cc0a4b16e163b862cab420d41309d
guotg5 <67358690+guotg5@users.noreply.github.com>
2020-12-15 19:16:30 +0800

    修改为maven工程
    Signed-off-by: guotg5 <guotg5@yonyou.com>

---
+++ src/main/java/com/guotg/depository/DepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.guotg.depository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guotg/depository/DepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guotg.depository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
